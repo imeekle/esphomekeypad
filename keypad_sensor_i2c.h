fc4fdfab085cdebbfba4b2d0d1c809a3ac9b78b4
Michael <imeekle@users.noreply.github.com>
2021-09-05 22:18:30 +1000

    Update and rename keypad_sensor_i2c to keypad_sensor_i2c.h

--- keypad_sensor_i2c.h
+++ keypad_sensor_i2c.h
@@ -9,11 +9,11 @@ class KeypadSensor : public Component, public Sensor {
 
     public:
 
-    const byte ROWS = 4; // Set the number of Rows
-    const byte COLS = 4; // Set the number of Columns
+    const byte n_rows = 4; // Set the number of Rows
+    const byte n_cols = 4; // Set the number of Columns
 
     // Set the Key at Use (4x4)
-    char keys [ROWS] [COLS] = {
+    char keys[n_rows] [n_cols] = {
       {'1', '4', '7', '/'},
       {'2', '5', '8', '0'},
       {'3', '6', '9', '.'},
@@ -21,8 +21,8 @@ class KeypadSensor : public Component, public Sensor {
     };
 
     // define active Pin (4x4)
-    byte rowPins [ROWS] = {0, 1, 2, 3}; // Connect to Keyboard Row Pin
-    byte colPins [COLS] = {4, 5, 6, 7}; // Connect to Pin column of keypad.
+    byte rowPins[n_rows] = {0, 1, 2, 3}; // Connect to Keyboard Row Pin
+    byte colPins[n_cols] = {4, 5, 6, 7}; // Connect to Pin column of keypad.
 
     // makeKeymap (keys): Define Keymap
     // rowPins:Set Pin to Keyboard Row
@@ -31,7 +31,7 @@ class KeypadSensor : public Component, public Sensor {
     // COLS: Set the number of Columns
     // I2CADDR: Set the Address for i2C
     // PCF8574: Set the number IC
-    Keypad_I2C keypad (makeKeymap (keys), rowPins, colPins, ROWS, COLS, I2CADDR, PCF8574);
+    Keypad_I2C myKeypad = Keypad(makeKeymap (keys), rowPins, colPins, n_rows, n_cols, I2CADDR, PCF8574);
 
     void setup() override {
     // This will be called by App.setup()
